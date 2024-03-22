f3d82fb3f3a31e55e89925b0d563b0c715add164
JiangWendyyy <j16698507162@163.com>
2024-03-22 20:54:01 +0800

    feat:add other modified files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import com.thoughtworks.kotlin_assignment.Entity.DataProduct
 import com.thoughtworks.kotlin_assignment.Entity.HighDemandProduct
 import com.thoughtworks.kotlin_assignment.Entity.NormalProduct
 import com.thoughtworks.kotlin_assignment.Entity.Product
@@ -9,22 +10,44 @@ suspend fun main() {
     val service = RetrofitClient.getInstance().create(ProductApi::class.java)
 
     // 发起 GET 请求获取产品列表
-    val allProducts = service.getAllProducts()
-    if(allProducts.isSuccessful) {
-        var body = allProducts.body()
+    val allDataProducts = service.getAllProducts()
+    if (service.getAllProducts().isSuccessful) {
+        val body = allDataProducts.body()
         if (!body.isNullOrEmpty()) {
-            val highDemandProducts = body.filter { it.type == "HIGH_DEMAND" }.map { HighDemandProduct(it.id,it.SKU,it.name,it.price,it.type,it.image)}
-            val normalProduct = body.filter { it.type == "NORMAL" }.map { NormalProduct(it.id,it.SKU,it.name,it.price,it.type,it.image) }
-            highDemandProducts.forEach { it.calculatePrice() }
-            display(highDemandProducts)
-            display(normalProduct)
+            val products = convertToProduct(body)
+            val highDemandProducts = products.filterIsInstance<HighDemandProduct>()
+            highDemandProducts.forEach {
+                val calculatePrice = it.calculatePrice()
+                it.setCalculatedPrice(calculatePrice)
+            }
+            display(products)
         }
-    }else{
+    } else {
         println("Error")
     }
 }
 
-private fun display(products: List<Product>) {
+fun convertToProduct(dataProduct: List<DataProduct>): List<Product> {
+    return dataProduct.map {
+        val product = when (it.type) {
+            "NORMAL" -> NormalProduct(
+                id = it.id, SKU = it.SKU, name = it.name, price = it.price, image = it.image, type = "NORMAL"
+            )
+
+            "HIGH_DEMAND" -> HighDemandProduct(
+                id = it.id, SKU = it.SKU, name = it.name, price = it.price, image = it.image, type = "HIGH_DEMAND"
+            )
+
+            else -> Product(
+                id = it.id, SKU = it.SKU, name = it.name, price = it.price, image = it.image, type = it.type
+            )
+        }
+        product
+    }.toList()
+}
+
+
+fun display(products: List<Product>) {
     for (product in products) {
         println(product.displayProductDetails())
     }
